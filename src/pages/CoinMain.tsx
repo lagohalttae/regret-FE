fadc88dfb5dfd97cc4d64c1768e9b9ade3072b60
woo-jk <201702042@o.cnu.ac.kr>
2022-05-25 04:02:41 +0900

    style: 메인 페이지 디자인 완성

--- src/pages/CoinMain.tsx
+++ src/pages/CoinMain.tsx
@@ -1,4 +1,47 @@
+import styled from 'styled-components';
+import MainTitle from '../components/mainTitle';
+import CoinInfo from '../components/coinInfo';
+import pepeImage from '../images/pepe.png';
+// const coinList = [
+//   {
+//     coins: [
+//       {
+//         coinId: 'bitcoin',
+//         label: '비트코인',
+//       },
+//       {
+//         coinId: 'etherium',
+//         label: '이더리움',
+//       },
+//     ],
+//   },
+// ];
+
+const Wrapper = styled.header`
+  position: fixed;
+  width: 100%;
+  font-size: 15px;
+  color: #000;
+  height: 80px;
+  background-color: #fff;
+  font-family: Noto Sans KR;
+`;
+
+const PepeImage = styled.img`
+  transform: scaleX(-1);
+  height: 100vh;
+  position: absolute;
+  right: -15%;
+`;
+
 function CoinMain(): any {
-  return <div>.</div>;
+  return (
+    <Wrapper>
+      <MainTitle />
+      <CoinInfo />
+      <PepeImage src={pepeImage} alt="pepe" />
+    </Wrapper>
+  );
 }
+
 export default CoinMain;
