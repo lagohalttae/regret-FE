898a545e4b3222e406e4d42b8461ab4b8dac7f23
woo-jk <201702042@o.cnu.ac.kr>
2022-07-08 01:11:22 +0900

    fix: api.ts

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-export const getCoins = async (): Promise<void> => {
-  const { data } = await axios.get(`${process.env.REACT_APP_API_URL}/coins/titles`, {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
+export const getCoins: any = () => {
+  axios.get(`${process.env.REACT_APP_API_URL}/coins/titles`).then((response) => {
+    console.log(response);
   });
-  console.log(data);
 };
