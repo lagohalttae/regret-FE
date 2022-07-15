053d767d901cdc1bd08431cf1c3173a3a9580453
woo-jk <201702042@o.cnu.ac.kr>
2022-07-16 02:17:42 +0900

    style: price 페이지 style 적용

--- src/components/price/index.tsx
+++ src/components/price/index.tsx
@@ -5,22 +5,52 @@ import { ICoinPrice, ICoinCurrentPrice } from '../../interface/coin';
 
 const Wrapper = styled.div`
   position: absolute;
-  top: 50vh;
-  left: 8vw;
-  @media (max-width: 480px) {
-    top: 35vh;
-    font-size: 8vw;
-  }
+  width: 80%;
+  margin: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 `;
-const Date1 = styled.div`
-  font-size: 2.6vw;
+
+const CurrentPrice = styled.div`
+  text-align: center;
+  margin-bottom: 14vh;
+`;
+
+const CurrentPriceInfo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 2vh;
+  padding: 0 1.5vw;
+`;
+
+const CurrentPriceTitle = styled.div`
+  font-size: 2vw;
+  font-weight: 700;
+`;
+
+const CurrentPriceText = styled.div`
+  font-size: 10.5vw;
+  font-weight: 700;
+  color: #43841f;
+`;
+
+const PastPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const PastDateText = styled.p`
+  font-size: 3.2vw;
   font-weight: 700;
   @media (max-width: 480px) {
     font-size: 7vw;
   }
 `;
-const Price1 = styled.div`
-  font-size: 2.3vw;
+
+const PastPriceText = styled.p`
+  font-size: 3.7vw;
   font-weight: 700;
   margin: 2vh 0 2vh 0;
   color: ${(props) => props.color};
@@ -28,16 +58,16 @@ const Price1 = styled.div`
     font-size: 7vw;
   }
 `;
-const Term = styled.div`
-  font-size: 1.3vw;
+
+const TermText = styled.p`
+  font-size: 1.6vw;
   color: lightgray;
   @media (max-width: 480px) {
     font-size: 5vw;
   }
 `;
-const Lowest = styled.div`
-  margin-bottom: 8vh;
-`;
+
+const Lowest = styled.div``;
 const Highest = styled.div``;
 
 function Price(): any {
@@ -45,6 +75,7 @@ function Price(): any {
   const [coinCurrentPrice, setCoinCurrentPrice] = useState<ICoinCurrentPrice>();
 
   useEffect(() => {
+    // TODO 임시로 비트코인으로 설정, 윗 페이지에서 설정한 코인으로 수정 필요
     getCoinPrice(setCoinPrice, 'bitcoin');
     getCoinCurrentPrice(setCoinCurrentPrice, 'bitcoin');
   }, []);
@@ -57,27 +88,39 @@ function Price(): any {
   const monthAgo = dateToString(new Date(now.setMonth(now.getMonth() - 1)));
   return (
     <Wrapper>
-      <div>현재 가격</div>
-      <div>{coinCurrentPrice?.price.won.toLocaleString()}KRW</div>
-      <div>{coinCurrentPrice ? new Date(coinCurrentPrice.lastUpdated).toLocaleString() : ''}</div>
-      <Lowest>
-        <Date1>
-          {coinPrice ? dateToString(new Date(coinPrice.minPrice.atMillis)) : ''}에 살걸..
-        </Date1>
-        <Price1 color="#E92C2C">{coinPrice?.minPrice.won.toLocaleString()}KRW</Price1>
-        <Term>
-          {monthAgo} ~ {today} 간 최저가
-        </Term>
-      </Lowest>
-      <Highest>
-        <Date1>
-          {coinPrice ? dateToString(new Date(coinPrice.maxPrice.atMillis)) : ''}에 팔걸..
-        </Date1>
-        <Price1 color="#0085FF">{coinPrice?.maxPrice.won.toLocaleString()}KRW</Price1>
-        <Term>
-          {monthAgo} ~ {today} 간 최고가
-        </Term>
-      </Highest>
+      <CurrentPrice>
+        <CurrentPriceInfo>
+          <CurrentPriceTitle>{coinCurrentPrice?.label} 현재 가격</CurrentPriceTitle>
+          <TermText>
+            {coinCurrentPrice ? new Date(coinCurrentPrice.lastUpdated).toLocaleString() : ''}
+          </TermText>
+        </CurrentPriceInfo>
+        <CurrentPriceText>{coinCurrentPrice?.price.won.toLocaleString()}KRW</CurrentPriceText>
+      </CurrentPrice>
+      <PastPrice>
+        <Lowest>
+          <PastDateText>
+            {coinPrice ? dateToString(new Date(coinPrice.minPrice.atMillis)) : ''}에 살걸..
+          </PastDateText>
+          <PastPriceText color="#E92C2C">
+            {coinPrice?.minPrice.won.toLocaleString()}KRW
+          </PastPriceText>
+          <TermText>
+            {monthAgo} ~ {today} 간 최저가
+          </TermText>
+        </Lowest>
+        <Highest>
+          <PastDateText>
+            {coinPrice ? dateToString(new Date(coinPrice.maxPrice.atMillis)) : ''}에 팔걸..
+          </PastDateText>
+          <PastPriceText color="#0085FF">
+            {coinPrice?.maxPrice.won.toLocaleString()}KRW
+          </PastPriceText>
+          <TermText>
+            {monthAgo} ~ {today} 간 최고가
+          </TermText>
+        </Highest>
+      </PastPrice>
     </Wrapper>
   );
 }
