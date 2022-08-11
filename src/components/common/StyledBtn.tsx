2a84213eec9e69f0864418c2cc67e64e27fd26a9
YimKeul <leesjun29@gmail.com>
2022-08-12 02:30:37 +0900

    refactor : add StyledBtn component

---
+++ src/components/common/StyledBtn.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  align-items: center;
+  padding: 10px;
+  max-height: 120px;
+  border-radius: 50px;
+  font-size: 2.5vw;
+  background-color: white;
+  border-color: #090909;
+  border-width: 2px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export default StyledBtn;
