9cf6cd6533483c5c7a0f2a29f8463ab95a03dfef
rladbrua0207 <rladbrua0207@gmail.com>
2022-08-13 01:20:02 +0900

    refactor: api 관련 parameter명 변경

--- src/api.ts
+++ src/api.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
 const baseUrl = 'http://localhost:8080'; // 로컬환경 아닐때: process.env.REACT_APP_API_URL
-const subUrl = (url: string, id?: string) =>
+const subUrl = (urlName: string, id?: string) =>
   ({
     coinList: '/coins/titles',
     coinPrice: `/coins/${id}`,
     coinCurrentPrice: `/coins/${id}/current`,
-  }[url]);
+  }[urlName]);
 
-export const axiosGet = async (url: string, id?: string, sendData?: any) => {
+export const axiosGet = async (urlName: string, id?: string, sendData?: any) => {
   try {
-    const response = await axios.get(`${baseUrl}${subUrl(url, id)}`, {
+    const response = await axios.get(`${baseUrl}${subUrl(urlName, id)}`, {
       params: sendData,
     });
     console.log(response.data);
@@ -21,9 +21,9 @@ export const axiosGet = async (url: string, id?: string, sendData?: any) => {
   return null;
 };
 
-export const axiosPost = async (url: string, id?: string, sendData?: any) => {
+export const axiosPost = async (urlName: string, id?: string, sendData?: any) => {
   try {
-    const response = await axios.post(`${baseUrl}${subUrl(url, id)}`, sendData);
+    const response = await axios.post(`${baseUrl}${subUrl(urlName, id)}`, sendData);
     console.log(response.data);
     return response.data;
   } catch (err) {
