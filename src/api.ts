a4b94e9735ac6eb097fea85f504a73f67e4a86e7
rladbrua0207 <rladbrua0207@gmail.com>
2022-08-13 00:58:08 +0900

    chore: api.ts 주석코드 삭제

--- src/api.ts
+++ src/api.ts
@@ -31,24 +31,3 @@ export const axiosPost = async (url: string, id?: string, sendData?: any) => {
   }
   return null;
 };
-
-// export const getCoins = (setCoinList: any): void => {
-//   axios.get(`${process.env.REACT_APP_API_URL}/coins/titles`).then((response) => {
-//     console.log(response.data);
-//     setCoinList(response.data);
-//   });
-// };
-
-// export const getCoinPrice = (setCoinPrice: any, id?: ISelectedCoin): void => {
-//   axios.get(`${process.env.REACT_APP_API_URL}/coins/${id}`).then((response) => {
-//     console.log(response.data);
-//     setCoinPrice(response.data);
-//   });
-// };
-
-// export const getCoinCurrentPrice = (setCoinCurrentPrice: any, id: ISelectedCoin): void => {
-//   axios.get(`${process.env.REACT_APP_API_URL}/coins/${id}/current`).then((response) => {
-//     console.log(response.data);
-//     setCoinCurrentPrice(response.data);
-//   });
-// };
