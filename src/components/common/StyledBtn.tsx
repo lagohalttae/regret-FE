463752278e7c6edbc94370b12306f21d797a5156
YimKeul <leesjun29@gmail.com>
2022-08-12 15:43:29 +0900

    feat : add type in StyledBtn

--- src/components/common/StyledBtn.tsx
+++ src/components/common/StyledBtn.tsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
-const StyledBtn = styled.button`
+type StyleBtnProps = {
+  size?: '1' | '2' | '4' | '6' | '10';
+  weight?: '400' | '500' | '600' | '700' | '900';
+  color?: string;
+};
+
+const StyledBtn = styled.button<StyleBtnProps>`
   align-items: center;
+  font-size: ${(props) => props.size || 2.5}vw;
+  font-weight: ${(props) => props.weight || '500'};
+  color: ${(props) => props.color || 'black'};
   padding: 10px;
   max-height: 120px;
   border-radius: 50px;
-  font-size: 2.5vw;
   background-color: white;
   border-color: #090909;
   border-width: 2px;
