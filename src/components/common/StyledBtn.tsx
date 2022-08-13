30572b1fd2ba3b4874fdd5e0b218323fa5dd08d3
YimKeul <leesjun29@gmail.com>
2022-08-14 02:10:31 +0900

    feat : StyledBtn, test code in Temp.tsx

--- src/components/common/StyledBtn.tsx
+++ src/components/common/StyledBtn.tsx
@@ -1,25 +1,67 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-type StyleBtnProps = {
-  size?: '1' | '2' | '4' | '6' | '10';
-  weight?: '400' | '500' | '600' | '700' | '900';
-  color?: string;
+type ButtonProps = {
+  size?: 'sm' | 'md' | 'lg';
+  color?: 'primary' | 'secondary';
+  disabled?: boolean;
 };
 
-const StyledBtn = styled.button<StyleBtnProps>`
-  align-items: center;
-  font-size: ${(props) => props.size || 2.5}vw;
-  font-weight: ${(props) => props.weight || '500'};
-  color: ${(props) => props.color || 'black'};
-  padding: 10px;
-  max-height: 120px;
-  border-radius: 50px;
-  background-color: white;
-  border-color: #090909;
-  border-width: 2px;
+const SIZE = {
+  sm: css`
+    font-size: 0.875rem;
+    padding: 8px 12px;
+    border-radius: 4px;
+  `,
+  md: css`
+    font-size: 1rem;
+    padding: 12px 16px;
+    border-radius: 8px;
+  `,
+  lg: css`
+    font-size: 1.25rem;
+    padding: 16px 20px;
+    border-radius: 12px;
+  `,
+};
+
+const COLOR = {
+  primary: css`
+    color: #fff;
+    background: linear-gradient(#3f3cfe, #e943d5);
+    &:hover {
+      background: linear-gradient(#2421f9, #e012c7);
+    }
+  `,
+  secondary: css`
+    color: #000;
+    background: linear-gradient(#c7c7d2, #bcbaba);
+    &:hover {
+      background: linear-gradient(#bdbdcf, #bcb1b1);
+    }
+  `,
+};
+
+const DISABLED = css`
+  cursor: not-allowed;
+  background: #d4d4d4;
+  color: #f5f5f5;
   &:hover {
-    cursor: pointer;
+    background: #d4d4d4;
+    color: #f5f5f5;
+    box-shadow: none;
   }
 `;
 
-export default StyledBtn;
+export const StyledBtn = styled.button<ButtonProps>`
+  cursor: pointer;
+  border: none;
+  font-weight: 500;
+  outline: none;
+  transition: all 0.2s;
+  font-size: 1rem;
+  padding: 12px 16px;
+  border-radius: 8px;
+  ${(props) => props.color && COLOR[props.color]}
+  ${(props) => props.size && SIZE[props.size]}
+  ${(props) => props.disabled && DISABLED}
+`;
--- src/pages/Temp.tsx
+++ src/pages/Temp.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import Typography from '../components/common/Typography';
-import StyledBtn from '../components/common/StyledBtn';
+import { StyledBtn } from '../components/common/StyledBtn';
 import { GreenColor } from '../constants';
 
 const S = {
@@ -19,7 +19,17 @@ const S = {
 function Temp(): any {
   return (
     <S.Container>
-      <StyledBtn color={GreenColor}>Hello</StyledBtn>
+      <StyledBtn color="primary" size="sm">
+        Hele
+      </StyledBtn>
+      <StyledBtn color="primary" size="md">
+        Hele
+      </StyledBtn>
+      <StyledBtn size="lg">Hele</StyledBtn>
+      <StyledBtn color="primary" disabled>
+        Hele
+      </StyledBtn>
+
       <S.TempBox>styled component 사용법</S.TempBox>
       <Typography weight="700" color={GreenColor}>
         타이포그래피 사용법
