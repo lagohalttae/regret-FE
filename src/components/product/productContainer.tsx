c8e053f602f761813f3fa12d4b76b3d95f1992d3
woo-jk <201702042@o.cnu.ac.kr>
2022-08-25 20:22:49 +0900

    refactor: Product 페이지 S 객체 생성

--- src/components/product/productContainer.tsx
+++ src/components/product/productContainer.tsx
@@ -1,23 +1,25 @@
 import styled from 'styled-components';
 import ViewportTypography from '../common/ViewportTypography';
 
-const Wrapper = styled.div`
-  position: relative;
-  height: 150vh;
-`;
+const S = {
+  Container: styled.div`
+    position: relative;
+    height: 150vh;
+  `,
 
-const ProductText = styled.p`
-  position: absolute;
-  top: 24vh;
-  left: 3vw;
-`;
-const ProductImg = styled.img`
-  position: absolute;
-  top: 40vh;
-  left: 12vw;
-  width: 28vw;
-`;
+  ProductText: styled.p`
+    position: absolute;
+    top: 24vh;
+    left: 3vw;
+  `,
 
+  ProductImg: styled.img`
+    position: absolute;
+    top: 40vh;
+    left: 12vw;
+    width: 28vw;
+  `,
+};
 type ProductContainerProps = {
   productName: string;
   unit: string;
@@ -27,8 +29,8 @@ type ProductContainerProps = {
 
 export function ProductContainer({ productName, unit, num, img }: ProductContainerProps) {
   return (
-    <Wrapper>
-      <ProductText>
+    <S.Container>
+      <S.ProductText>
         <ViewportTypography size="4" weight="700" marginBottom={20}>
           {productName}
         </ViewportTypography>
@@ -36,8 +38,8 @@ export function ProductContainer({ productName, unit, num, img }: ProductContain
           {num.toLocaleString()}
           {unit}을(를) 살 수 있었을텐데..
         </ViewportTypography>
-      </ProductText>
-      <ProductImg src={img} />
-    </Wrapper>
+      </S.ProductText>
+      <S.ProductImg src={img} />
+    </S.Container>
   );
 }
